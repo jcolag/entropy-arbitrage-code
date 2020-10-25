0a35078416a46ad1b65bc95a6710301f16cda67d
John Colagioia <john@colagioia.net>
2020-10-25 06:17:20 -0400

    Parameterize the number of days in advance to open

--- open.sh
+++ open.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 day=0
+max=7
+if [ $# -ge 1 ]
+then
+  max=$1
+fi
 pluma _sass/minima.scss
-while [ $day -le 7 ]
+while [ $day -le $max ]
 do
   dd=$(faketime -f "+${day}d" date +%F)
   file=$(ls -1 _posts/${dd}-*.md 2> /dev/null)
