90a84c6175fba48955a3ef37b5490b200cbbd3fa
John Colagioia <john@colagioia.net>
2021-03-23 06:16:32 -0400

    Clean up grammar checking script

--- check.sh
+++ check.sh
@@ -2,27 +2,27 @@
 
 # Get the date or other prefix
 day=$(date +'%Y-%m-%d')
-files="${HOME}/code/entropy-arbitrage-code/_posts/$day*.md"
-if [ .$1. != .. ]
+files="${HOME}/code/entropy-arbitrage-code/_posts/${day}*.md"
+if [ ".$1." != ".." ]
 then
-  files=$@
+  files=$*
 fi
 
-echo $files
-if case $(echo $files | tr '*' '#') in *#*) ;; *) false;; esac; then
+if case $(echo "${files}" | tr '*' '#') in *#*) ;; *) false;; esac; then
   exit
 fi
 
+echo "${files}"
 mv "${HOME}/proselintrc.bk" "${HOME}/.proselintrc"
-for file in $files
+for file in ${files}
 do
   outfile=$(mktemp).json
-  proselint --json "$file" | jq .data.errors > "$outfile"
+  proselint --json "${file}" | jq .data.errors > "${outfile}"
   java -jar "${HOME}/bin/LanguageTool-5.2-stable/languagetool-commandline.jar" \
     --disablecategories TYPOGRAPHY \
     --encoding utf-8 --json --language en-US --xmlfilter \
-    "$file" | jq .matches >> "$outfile"
-  pluma $outfile
+    "${file}" | jq .matches >> "${outfile}"
+  pluma "${outfile}"
 done
 
 mv "${HOME}/.proselintrc" "${HOME}/proselintrc.bk"
