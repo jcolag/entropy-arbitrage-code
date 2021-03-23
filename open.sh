e7e51603f4192e9e63c5a707ac44e8b4719bb4c0
John Colagioia <john@colagioia.net>
2021-03-23 06:21:37 -0400

    Clean up mass-open script

--- open.sh
+++ open.sh
@@ -6,14 +6,14 @@ then
   max=$1
 fi
 pluma _sass/minima.scss
-while [ $day -le $max ]
+while [ "$day" -le "$max" ]
 do
   dd=$(faketime -f "+${day}d" date +%F)
-  file=$(ls -1 _posts/${dd}-*.md 2> /dev/null)
-  if [ ! -z $file ]
+  file=$(ls -1 _posts/"${dd}"-*.md 2> /dev/null)
+  if [ -n "$file" ]
   then
-    pluma $file
-    sleep 0.5
+    pluma "$file"
+    sleep 0.3
   fi
   day=$((day + 1))
 done
