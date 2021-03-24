076913aeec64cac572ac20bfebb7382beab6a33e
John Colagioia <john@colagioia.net>
2021-03-24 06:26:47 -0400

    Normalize date usage

--- open.sh
+++ open.sh
@@ -8,7 +8,7 @@ fi
 pluma _sass/minima.scss
 while [ "$day" -le "$max" ]
 do
-  dd=$(faketime -f "+${day}d" date +%F)
+  dd=$(date --date "+${day} days" +%F)
   file=$(ls -1 _posts/"${dd}"-*.md 2> /dev/null)
   if [ -n "$file" ]
   then
