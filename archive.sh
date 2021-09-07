9598f750adc6dfb099daa7baf7aabb5ea6f9557f
John Colagioia <john@colagioia.net>
2021-09-07 06:50:23 -0400

    Add prototype Internet Archive script

---
+++ archive.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+dom=$(date +%_d)
+if [ ${dom} -gt 7 ] || [ .$(date +%a). != .Sat. ]
+then
+  exit 0
+fi
+server=$(grep '^url: ' _config.yml | cut -f2 -d'"')
+path=$(grep '^baseurl: ' _config.yml | cut -f2 -d'"')
+sitemap="${server}${path}/sitemap.xml"
+local=$(mktemp --suffix=.xml)
+
+curl "${sitemap}" >> "${local}"
+if [ ".$1." != ".." ]
+then
+  local=$*
+fi
+
+for link in $(grep '<loc>' "${local}" | cut -f2- -d'>' | cut -f1 -d'<')
+do
+  echo "${link}"
+  curl -s "https://web.archive.org/save/${link}"
+  sleep 6
+done
+
