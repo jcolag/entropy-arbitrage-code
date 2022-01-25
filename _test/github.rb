b779d5e4ea542586d2c8de9d13ec5e075e48c233
John Colagioia <john@colagioia.net>
2022-01-25 06:16:59 -0500

    Add option to trace GitHub test

--- _test/github.rb
+++ _test/github.rb
@@ -2,10 +2,13 @@ require 'liquid'
 require 'tracer'
 require_relative '../_plugins/github.rb'
 
+Tracer.on unless ENV['DEBUG'].nil?
+
 repo = 'jcolag/dash'
 repo = ARGV[0] unless ARGV.empty?
 context = Liquid::ParseContext.new
 ght = GithubInlineTag.send :new, 'github', repo, context
 
 puts ght.render(context)
+Tracer.off
 
