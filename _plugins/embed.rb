053306557136c0ef351c3660aff5f6c63cd00e96
John Colagioia <john@colagioia.net>
2023-02-06 06:50:17 -0500

    Fix the embed tag

--- _plugins/embed.rb
+++ _plugins/embed.rb
@@ -1,26 +1,31 @@
 # frozen_string_literal: true
 
 # Code for the citation inline tag
-class SocialImageTag < Liquid::Tag
+class SocialImageInlineTag < Liquid::Tag
   def initialize(tag_name, text, parse_context)
     super
     @text = text
   end
 
   def render(_context)
-    url, description, is_sensitive, credit = params @text
-    return '' if credit.empty?
+    url, description, sensitive, credit = params @text
+    if credit.empty?
+      return "<p class='no-image'><i class='fas fa-image'></i>" \
+        " Image Not Shown: #{description}</p>"
+    end
 
-    css_class = "embedded#{is_sensitive ? ' sensitive' : ''}"
-    "<a href='#{url}'><figure><img alt='#{description}' " \
-      "class='#{css_class}' src='#{url} title='#{description}'>" \
-      "<figcaption>#{credit}</figcaption></a>"
+    css_class = "embedded#{sensitive ? ' sensitive' : ''}"
+    "<figure class='external-image'><img alt='#{description}' " \
+      "class='#{css_class}' src='#{url}' title='#{description}'>" \
+      "<figcaption>#{description}<br>Image credit: #{credit}" \
+      "</figcaption></figure>"
   end
 
   def params(input)
     parts = input.split '|'
-    [parts[0].strip, parts[1].strip, parts.length > 2 ? parts[2].strip : false,
-     parts.length > 3 ? parts[3].strip : '']
+    [parts[0].strip, CGI::escapeHTML(parts[1].strip),
+      parts.length > 2 ? parts[2].strip == 'true' : false,
+      parts.length > 3 ? CGI::escapeHTML(parts[3].strip) : '']
   end
 end
-Liquid::Template.register_tag('embed', SocialImageTag)
+Liquid::Template.register_tag('embed', SocialImageInlineTag)
