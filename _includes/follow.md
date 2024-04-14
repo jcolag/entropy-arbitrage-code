f4fa355c2ba0a4d914e822ae0d7db209f2a7dadd
John Colagioia <john@colagioia.net>
2024-04-14 06:33:16 -0400

    Replace recurring boilerplate text with an _include

---
+++ _includes/follow.md
@@ -0,0 +1,4 @@
+## Follow Me
+
+If you appreciate this sort of content, then you should probably [follow me <i class="fab fa-mastodon"></i> on Mastodon](https://mastodon.social/@jcolag/) to get it as early as possible...and feel free to reply, at least to the good stuff.
+
--- nextweek.sh
+++ nextweek.sh
@@ -112,9 +112,7 @@ Because it accidentally became a tradition early on in the life of the blog, I d
 
 
 
-## Follow Me
-
-If you appreciate this sort of content, then you should probably [follow me <i class="fab fa-mastodon"></i> on Mastodon](https://mastodon.social/@jcolag/) to get it as early as possible...and feel free to reply, at least to the good stuff.
+{% include follow.md %}
 
 * * *
 
