30c26dda38307c8ade83f66ba03b9aeaec473e12
John Colagioia <john@colagioia.net>
2024-07-09 06:37:55 -0400

    Add plugin like nest, but that takes a variable

---
+++ _plugins/nest_index.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+# Code for the nest inline tag, which accepts a variable
+class NestIndexInlineTag < Liquid::Tag
+  def initialize(tag_name, text, parse_context)
+    super
+    @post_id = text.strip
+puts @post_id
+  end
+
+  def render(_context)
+    site = _context.registers[:site]
+    post_id = _context[@post_id]
+    post = site.posts.docs.find { |p| p.basename_without_ext == post_id }
+    converter = site.find_converter_instance ::Jekyll::Converters::Markdown
+    layout = site.layouts[post.data['layout']]
+
+    if post
+      content = converter.convert post.content
+      _context.stack do
+        _context['page'] = Jekyll::Drops::DocumentDrop.new post
+        _context['content'] = content
+        Liquid::Template.parse(layout.content).render _context
+      end
+    else
+      "No post <tt>#{@post_id}</tt> found!"
+    end
+  end
+end
+Liquid::Template.register_tag('nest_index', NestIndexInlineTag)
