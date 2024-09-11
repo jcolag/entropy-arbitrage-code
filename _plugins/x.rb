cc776f21265b207a505dde6646ab544d2a70279e
John Colagioia <john@colagioia.net>
2024-09-11 06:54:42 -0400

    Add plugin to consistently refer to X (formerly Twitter)

---
+++ _plugins/x.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+# Code for the keyboard inline tag
+class XInlineTag < Liquid::Tag
+  def initialize(tag_name, text, parse_context)
+    super
+    @text = text
+  end
+
+  def render(_context)
+    "<span class=\"x\" title=\"née Twitter\">&#x1d54f;</span>"
+  end
+end
+Liquid::Template.register_tag('x', XInlineTag)
