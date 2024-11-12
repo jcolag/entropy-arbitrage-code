c5c4c60eef5a994a8fef04ca60fd859d0afd3270
John Colagioia <john@colagioia.net>
2024-11-12 06:46:58 -0500

    Add Creative Commons Unicode plugin

---
+++ _plugins/cc.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+require 'json'
+
+# Code for the citation inline tag
+class CcInlineTag < Liquid::Tag
+  def initialize(tag_name, text, parse_context)
+    super
+    @text = text.strip.downcase
+  end
+
+  def render(_context)
+    licenses = {
+      '' => '&#xa9;',
+      '0' => '&#x1f16d;&#x1f10d;',
+      'by' => '&#x1f16d;&#x1f16f;',
+      'by-sa' => '&#x1f16d;&#x1f16f;&#x1f10e;',
+      'by-nc' => '&#x1f16d;&#x1f16f;&#x1f10f;',
+      'by-nc-sa' => '&#x1f16d;&#x1f16f;&#x1f10f;&#x1f10e;',
+      'by-nd' => '&#x1f16d;&#x1f16f;&#x229C;',
+      'by-nc-nd' => '&#x1f16d;&#x1f16f;&#x1f10f;&#x229C;',
+      'pd' => '&#x1f16d;&#x1f16e;',
+    }
+    license = licenses[@text]
+
+    "<span class=\"license\" title=\"#{@text}\">#{license}</span>"
+  end
+end
+Liquid::Template.register_tag('cc', CcInlineTag)
