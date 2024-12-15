9e5395669df8ad90de316a6812f1eeb60dcb4ee6
John Colagioia <john@colagioia.net>
2024-12-15 06:47:58 -0500

    Revise content warning plugin to render Markdown as HTML

--- _plugins/cw.rb
+++ _plugins/cw.rb
@@ -1,15 +1,18 @@
 # frozen_string_literal: true
 
-# Code for the citation inline tag
+# Code for the content warning inline tag
 class ContentWarningTag < Liquid::Tag
   def initialize(tag_name, text, parse_context)
     super
     @text = text
   end
 
-  def render(_context)
-    "<p class='content-warning'><i class='fas fa-exclamation-triangle'>" \
-      "</i> <b>Content Warning</b>:  #{@text}</p>"
+  def render(context)
+    site = context.registers[:site]
+    converter = site.find_converter_instance(::Jekyll::Converters::Markdown)
+    @html = converter.convert(@text)
+    "<div class='content-warning'><i class='fas fa-exclamation-triangle'></i>" \
+      " <b>Content Warning</b>:  #{@html}</div>"
   end
 end
 Liquid::Template.register_tag('cw', ContentWarningTag)
