d0d8ad409a4d89ccce5b3f75204508a933ffb227
John Colagioia <john@colagioia.net>
2026-06-03 07:02:49 -0400

    Add filter to convert number to the ordinal word

--- _plugins/textfilters.rb
+++ _plugins/textfilters.rb
@@ -4,6 +4,22 @@
 module Jekyll
   # Filtering input text
   module TextFilter
+    # Only relevant numbers; should become more algorithmic for the
+    # general case.
+    TERMS = {
+      1 => 'first',
+      2 => 'second',
+      3 => 'third',
+      4 => 'fourth',
+      5 => 'fifth',
+      6 => 'sixth',
+      7 => 'seventh',
+      28 => 'twenty-eighth',
+      29 => 'twenty-ninth',
+      30 => 'thirtieth',
+      31 => 'thirty-first'
+    }.freeze
+
     # Code for Liquid to find the first of a given weekday in a month/year.
     # 0-6 for the day of the week
     def first_w_of_month(date, dow)
@@ -13,6 +29,11 @@ module Jekyll
       wday = day1.wday
 
       wday == dow + 1 ? day1 - 1 : day1 + (9 - wday)
+    end
+
+    # Convert 2 to "second," etc.
+    def ordinal_word(input)
+      TERMS[input.to_i]
     end
   end
 end
