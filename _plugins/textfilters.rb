33ed6b20c29872d6dfcfedb5cc078b17048c2344
John Colagioia <john@colagioia.net>
2026-06-10 06:50:48 -0400

    Fix math to catch edge-cases

--- _plugins/textfilters.rb
+++ _plugins/textfilters.rb
@@ -23,12 +23,10 @@ module Jekyll
     # Code for Liquid to find the first of a given weekday in a month/year.
     # 0-6 for the day of the week
     def first_w_of_month(date, dow)
-      date.year
-      next_month = date.month + 1
-      day1 = Date.new date.year, next_month, 1
+      day1 = Date.new date.year, date.month + 1, 1
       wday = day1.wday
 
-      wday == dow + 1 ? day1 - 1 : day1 + (9 - wday)
+      wday == (dow + 1) % 7 ? day1 - 1 : (day1 + dow + 7 - wday) % 7
     end
 
     # Convert 2 to "second," etc.
