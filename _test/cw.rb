c9e0df4d40b5bfb3888ce2697f8e898a028bffc6
John Colagioia <john@colagioia.net>
2026-09-09 06:59:23 -0400

    Separate context types

--- _test/cw.rb
+++ _test/cw.rb
@@ -1,3 +1,4 @@
+require 'jekyll'
 require 'liquid'
 require 'tracer'
 require_relative '../_plugins/cw.rb'
@@ -6,8 +7,10 @@ Tracer.on unless ENV['DEBUG'].nil?
 
 warn = 'This *should* test **Markdown** and "smart-quotes..."'
 warn = ARGV.join ' ' unless ARGV.empty?
-context = Liquid::ParseContext.new
-cw = ContentWarningTag.send :new, 'cw', warn, context
+p_context = Liquid::ParseContext.new
+cw = ContentWarningTag.send :new, 'cw', warn, p_context
 
+context = Liquid::Context.new
+context.registers[:site] = Jekyll::Site.new(Jekyll.configuration)
 puts cw.render(context)
 #Tracer.off
